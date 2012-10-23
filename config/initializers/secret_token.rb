64e3f4cf79e9268bc0076a293b39510aeea34be8
folia <foliaoreport@gmail.com>
2012-10-23 19:38:41 +0100

    mila1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mila::Application.config.secret_token = '540fa8c969e78f608c2a2e8cb647bd8d62763fb17f437d057913d79b14eb27e68f7ed475c47e26222b878103329703a99e9efc9c9b33a31886c6a8a17a42d3b7'
